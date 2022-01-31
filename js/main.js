df06107f928233688f5d17eab9c961324abce3a7
David Shin <shin.david93@gmail.com>
2022-01-31 07:35:32 -0800

    remove api key from link

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
 //  Quick access link to get API KEY https://home.openweathermap.org/api_keys
-const API_KEY = 'API_KEY'
+const API_KEY = 'API KEY GOES HERE'
 const card = document.getElementById('card');           //  Grabs bootstrap card from index.html
 
 /**
- * 
  * @param city 
  * @param zipcode 
  * @returns data 
  * Function grabs user-inputted city and zipcode and returns a JSON data file 
  */
 const getAPIData = async (city, zipcode=null) => {
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zipcode=${zipcode},us&q=${city}&appid=900eee3f4e6d3e40090ade1876b9fcc3&units=imperial`);
+    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?zipcode=${zipcode},us&q=${city}&appid=${API_KEY}&units=imperial`);
     let data = await response.json();
     return await data;
 }
 
 /**
- * 
  * @param data 
  * @param city 
  * Function gets the JSON data file and city from the weatherForm eventListener and prints out formatted data
@@ -35,13 +33,13 @@ const displayData = async (data, city) => {
     document.getElementById('card-title').innerText = await city.charAt(0).toUpperCase() + city.substr(1, city.length-1);
     img.src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`
     document.getElementById('card-temp').innerText = parseInt(data.main.temp) + '\u2109';
-    document.getElementById('card-text').innerHTML = `<em>Feels like: ${parseInt(data.main.feels_like)}'\u2109'</em>`;
+    document.getElementById('card-text').innerHTML = `<em>Feels like: ${parseInt(data.main.feels_like)}\u2109</em>`;
 
     //  Changes list elements in the card
-    li1.innerHTML = `<strong>High:</strong> ${parseInt(data.main.temp_max)}'\u2109'`
-    li2.innerHTML = `<strong>Low:</strong> ${parseInt(data.main.temp_min)}'\u2109'`
+    li1.innerHTML = `<strong>High:</strong> ${parseInt(data.main.temp_max)}\u2109`
+    li2.innerHTML = `<strong>Low:</strong> ${parseInt(data.main.temp_min)}\u2109`
     li3.innerHTML = `<strong>Pressure:</strong> ${parseInt(data.main.pressure)} psia`
-    li4.innerHTML = `<strong>Low:</strong> ${parseInt(data.main.humidity)}'\u2109'`
+    li4.innerHTML = `<strong>Low:</strong> ${parseInt(data.main.humidity)}\u2109`
 
 }
 
